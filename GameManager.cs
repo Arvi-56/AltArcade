b6c57e2ee0d6f9684bbf451f0a99f1caca78047a
Arvi-56 <58665353+Arvi-56@users.noreply.github.com>
2020-09-12 14:19:22 -0600

    Add files via upload
    These are the C# scripts used to make the basic pong game

---
+++ GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+     [Header("Ball")]
+     public GameObject Ball;
+
+     [Header("Player 1")]
+     public GameObject Player1;
+     public GameObject Player1Goal;
+
+     [Header("Player 2")]
+     public GameObject Player2;
+     public GameObject Player2Goal;
+
+     [Header("Score UI")]
+     public GameObject Player1Score;
+     public GameObject Player2Score;
+
+     private int Player1Text;
+     private int Player2Text;
+
+     public void Player1Scored()
+     {
+          Player1Text++;
+          Player1Score.GetComponent<TextMeshProUGUI>().text = Player1Text.ToString();
+          ResetPosition();
+     }
+     public void Player2Scored()
+     {
+          Player2Text++;
+          Player2Score.GetComponent<TextMeshProUGUI>().text = Player2Text.ToString();
+          ResetPosition();
+     }
+     private void ResetPosition()
+     {
+          Ball.GetComponent<Ball>().Reset();
+          Player1.GetComponent<Paddle>().Reset();
+          Player2.GetComponent<Paddle>().Reset();
+     }
+}
